e76938bd80e31e1d6144c282bd41a16dfcb43888
TUS-hollykretschmer-2022 <117652469+TUS-hollykretschmer-2022@users.noreply.github.com>
2023-01-31 14:46:48 +0000

    Update HingeController.cs

--- Assets/Scripts/HingeController.cs
+++ Assets/Scripts/HingeController.cs
@@ -7,22 +7,17 @@ public class HingeController : MonoBehaviour
 {
     private HingeJoint2D hinge;
 
-  // Start is called before the first frame update
-  void Start()
+    // Start is called before the first frame update
+    void Start()
     {
-      hinge = GetComponent<HingeJoint2D>();
+        hinge = GetComponent<HingeJoint2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
-
-   public void hingeSpeed(float SpeedToSet)
-    {
-        JointMotor2D theMotor = hinge.motor;
-        theMotor.motorSpeed = speedToSet;
-        hinge.motor = theMotor;
+        var motor = hinge.motor;
+        motor.motorSpeed = 0;
+            //hinge.motor = motor
     }
 }
